b018430a2f6a92eebaafb7b764b56f67cddce6f3
Ruth Christina <49425212+ruthchristinaa@users.noreply.github.com>
2021-03-09 19:12:34 +0700

    Add files via upload

---
+++ cara.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tesaurus Bahasa Indonesia</title>
+	<link rel="stylesheet" type="text/css" href="cara.css">
+</head>
+<body>
+	<div class="navigasi">
+		<ul>
+			<li><a href="tesaurusbaru.php">Tesaurus.id</a></li>
+			<li><a href="tentang.php">Tentang</a></li>
+			<li><a href="cara.php">Cara Penggunaan</a></li>
+		</ul>
+	</div>
+	<div class="artikel">
+		<h1>CARA PENGGUNAAN</h1>
+		<p id="cara" style="text-indent: 50px;">
+			Cara penggunaan dari Tesaurus.id yaitu menyediakan <i>Kata Dasar </i>yang memiliki hubungan kata atau disebut dengan <i> Relasi Kata.</i> Tesaurus.id juga menampilkan sinonim kata tesaurus yang berkaitan dengan label mengenai Ragam Bahasa, Kelas Kata dan Kata Serapan. Berikut adalah detail dari label dalam Tesaurus.id :
+			<br>
+			<br>
+		</p>
+
+		<p id="list">	
+			<b> 1. Ragam Bahasa , variasi bahasa sesuai konteks pemakaian</b>
+			<table>
+				<tr>
+					<td>- <i>ark </i> arkais</td>
+					<td>- <i>cak </i> cakapan</td>
+					<td>- <i>hor </i> hormat</td>
+					<td>- <i>ki </i> kiasan</td>
+					<td>- <i>kl </i> klasik</td>
+				</tr>
+			</table>
+			<br>
+		</p>
+		<p id="list"> 
+			<b> 2. Kelas Kata , kata yang memiliki fungsi dan makna</b>
+			<table>
+				<tr>
+					<td>- <i>a </i> adjektiva</td>
+					<td>- <i>adv </i> adverbia</td>
+					<td>- <i>n </i> nomina</td>
+					<td>- <i>num </i> numeralia</td>
+					<td>- <i>p </i> partikel</td>
+					<td>- <i>pron </i> pronomina</td>
+					<td>- <i>v </i> verba</td>
+				</tr>
+			</table>
+			<br>
+		</p>
+
+		<p id="list">
+			<b> 3. Kata Serapan , berasal dari bahasa daerah atau bahasa asing</b>
+			<table>
+				<tr>
+					<td>- <i>Ar </i> Arab</td>
+					<td>- <i>Bl </i> Bali</td>
+					<td>- <i>Bld </i> Belanda</td>
+					<td>- <i>Cn </i> Cina</td>
+					<td>- <i>Jk </i> Jakarta</td>
+					<td>- <i>Jw </i> Jawa</td>
+					<td>- <i>Mk </i> Minangkabau</td>
+					<td>- <i>Sd </i> Sunda</td>
+					<td>- <i>Skt </i> Sanskerta</td>
+				</tr>
+			</table>
+			<br>
+		</p>
+	</div>
+	<div class="footer">
+		<p> Universitas Kristen Duta Wacana</p>
+		<p> Fakultas Teknologi Informasi</p>
+		<p> Prodi Informatika</p>
+		<p> 2020</p>
+	</div>
+</body>
+</html>
