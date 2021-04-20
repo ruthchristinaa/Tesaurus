4a71c1a3378ed250ab421072f7eb6b7aa84ac993
Ruth Christina <49425212+ruthchristinaa@users.noreply.github.com>
2021-04-20 17:12:53 +0700

    Add files via upload

---
+++ tentang.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tesaurus Bahasa Indonesia</title>
+	<link rel="stylesheet" type="text/css" href="tentang.css">
+</head>
+<body>
+	<div class="navigasi">
+		<ul>
+			<li><a href="tesaurus.php">Tesaurus.id</a></li>
+			<li><a href="tentang.php">Tentang</a></li>
+			<li><a href="cara.php">Cara Penggunaan</a></li>
+		</ul>
+	</div>
+	<div class="artikel">
+		<h1>TENTANG</h1>
+		<p id="tentang" style="text-indent: 50px;">
+			Tesaurus.id merupakan tesaurus berbahasa Indonesia yang di desain dalam bentuk website supaya pengguna dapat mudah melakukan pencarian kata dibandingkan melalui buku atau kamus cetak. Tesaurus.id ini dapat menyajikan sebanyak mungkin lema yang berkaitan mengenai kata dasar, relasi kata imbuhan dan kelompok kata yang berelasi dengan setiap kata yang dijelaskan dengan label. Tesaurus ini dibuat dengan menampilkan data yang tersimpan di dalam representasi pengetahuan <i>Simple Knowledge Organization System</i> yang dibangun oleh Michael Himawan Handoko, Gloria Virginia dan Budi Susanto pada tahun 2015.
+		</p>
+	</div>
+	<div class="footer">
+		<p> Universitas Kristen Duta Wacana</p>
+		<p> Fakultas Teknologi Informasi</p>
+		<p> Prodi Informatika</p>
+		<p> 2020</p>
+	</div>
+</body>
+</html>
